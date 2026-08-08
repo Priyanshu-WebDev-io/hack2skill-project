28f434c13af4a232048abb734bd39c7a2129a615
priyanshu kumar <priyanshu.webdev.io@gmail.com>
2026-08-08 11:16:10 +0530

    fix: add oauth coop headers

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer-when-downgrade', // Ensure origin is passed to Google
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
